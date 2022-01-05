529f2d5f9d880fb565685f1b72e5256d28c5e9cd
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-04 19:16:28 -0500

    added html and styling

--- main.js
+++ main.js
@@ -16,15 +16,32 @@ async function login() {
      console.log(error)
    }
   }
+
+  showLoginControls()
 }
 
 async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
+  showLoginControls()
 }
 
-document.getElementById("btn-login").onclick = login;
+document.getElementById("login_button").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
+
+function showLoginControls(){
+    let user = Moralis.User.current();
+    if (user){
+        document.getElementById("login_button").style.display="none";
+        document.getElementById("btn-logout").style.display="block";
+    }
+    else{
+        document.getElementById("login_button").style.display="block";
+        document.getElementById("btn-logout").style.display="none";
+    }
+}
+
+showLoginControls();
 
 /** Useful Resources  */
 
